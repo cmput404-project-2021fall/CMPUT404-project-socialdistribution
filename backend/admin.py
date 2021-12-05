f232cf7308822f4d481ecb190442161a414be2c7
Jihoon Og <jihoonog@gmail.com>
2021-12-04 18:30:01 -0700

    Added debug info to admin page

--- backend/admin.py
+++ backend/admin.py
@@ -1,14 +1,6 @@
 from django.contrib import admin
 from .models import Author, Post, Comment, Like, FriendRequest, Inbox, Node
 
-# Register your models here.
-admin.site.register(Author)
-admin.site.register(Post)
-admin.site.register(Comment)
-admin.site.register(Like)
-admin.site.register(FriendRequest) 
-admin.site.register(Inbox)
-
 @admin.action(description="Set selected node(s) to require authentication")
 def set_require_auth(modeladmin, request, queryset):
     queryset.update(require_auth=True)
@@ -17,9 +9,35 @@ def set_require_auth(modeladmin, request, queryset):
 def set_no_require_auth(modeladmin, request, queryset):
     queryset.update(require_auth=False)
 
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = [Author.__str__, 'host']
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = [Post.__str__, 'origin', 'author']
+    ordering = ['title']
+
+class CommentAdmin(admin.ModelAdmin):
+    ordering = ['id', 'post']
+
+class InboxAdmin(admin.ModelAdmin):
+    list_display = ['id']
+
 class NodeAdmin(admin.ModelAdmin):
     list_display = ['host','require_auth']
     ordering = ['host']
     actions = [set_require_auth, set_no_require_auth]
 
+class LikeAdmin(admin.ModelAdmin):
+    list_display = ['author','object']
+
+class FriendRequestAdmin(admin.ModelAdmin):
+    list_display = ['actor', 'object']
+
+# Register your models here.
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Post, PostAdmin)
+admin.site.register(Comment, CommentAdmin)
+admin.site.register(Like, LikeAdmin)
+admin.site.register(FriendRequest, FriendRequestAdmin) 
+admin.site.register(Inbox, InboxAdmin)
 admin.site.register(Node, NodeAdmin)
