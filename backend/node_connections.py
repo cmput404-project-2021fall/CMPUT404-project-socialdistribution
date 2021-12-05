4078a3e510db1933e6bc3a6700e53a9eb9ad5146
Jihoon Og <jihoonog@gmail.com>
2021-12-04 22:53:46 -0700

    request fix

--- backend/node_connections.py
+++ backend/node_connections.py
@@ -191,7 +191,7 @@ def send_to_friends(author: Author, payload: dict):
             param_list.append(None)
             data_list.append(payload)
 
-        with ThreadPoolExecutor(max_workers=10) as pool:
+        with ThreadPoolExecutor(max_workers=2) as pool:
             res_friend_inbox_obj_list = list(pool.map(async_post, friend_url_list, auth_info_list, header_list, param_list, data_list))
             
     except Exception as e:
@@ -229,7 +229,7 @@ def send_friend_request(author: Author, payload: dict):
         param_list.append(None)
         data_list.append(payload)
 
-        with ThreadPoolExecutor(max_workers=10) as pool:
+        with ThreadPoolExecutor(max_workers=2) as pool:
             res_friend_inbox_obj_list = list(pool.map(async_post, friend_url_list, auth_info_list, header_list, param_list, data_list))
             
     except Exception as e:
@@ -267,7 +267,7 @@ def send_like(author: Author, payload: dict):
         param_list.append(None)
         data_list.append(payload)
 
-        with ThreadPoolExecutor(max_workers=10) as pool:
+        with ThreadPoolExecutor(max_workers=2) as pool:
             res_friend_inbox_obj_list = list(pool.map(async_post, friend_url_list, auth_info_list, header_list, param_list, data_list))
             
     except Exception as e:
@@ -286,7 +286,7 @@ def update_remote_posts(host: str, auth: str, foreign_author_ids: list, time_pro
         remote_authors_host = Author.objects.filter(id__in=foreign_author_ids).values_list('url', flat=True)
         post_dict_list = []
 
-        with ThreadPoolExecutor(max_workers=10) as pool:
+        with ThreadPoolExecutor(max_workers=2) as pool:
             urls = [author_url + '/posts/' for author_url in remote_authors_host]
             user, passwd = auth.split(':')
             auths = [(user, passwd)]*len(urls)
@@ -341,7 +341,7 @@ def update_remote_comments(host: str, auth: str, foreign_post_ids: list, time_pr
         remote_comment_url_post_id = Post.objects.filter(id__in=foreign_post_ids).values_list('comment_url', 'id')
         comment_dict_list = []
 
-        with ThreadPoolExecutor(max_workers=10) as pool:
+        with ThreadPoolExecutor(max_workers=2) as pool:
             urls = [comment_url_post_id[0] + '/' if 'plurr' in comment_url_post_id[0] else comment_url_post_id[0] for comment_url_post_id in remote_comment_url_post_id]
             post_objs = [Post.objects.get(id=comment_url_post_id[1]) for comment_url_post_id in remote_comment_url_post_id]
             user, passwd = auth.split(':')
@@ -395,7 +395,7 @@ def update_remote_likes(host: str, auth: str, foreign_author_ids: list, time_pro
         remote_authors_host = Author.objects.filter(id__in=foreign_author_ids).values_list('url', flat=True)
         likes_dict_list = []
 
-        with ThreadPoolExecutor(max_workers=10) as pool:
+        with ThreadPoolExecutor(max_workers=2) as pool:
             urls = [author_url + '/liked/' if 'plurr' in author_url else author_url + '/liked' for author_url in remote_authors_host]
             user, passwd = auth.split(':')
             auths = [(user, passwd)]*len(urls)
@@ -513,7 +513,7 @@ def update_remote_followers(host: str, auth: str, foreign_author_ids: list, time
     try:
         remote_authors_host = Author.objects.filter(id__in=foreign_author_ids).values_list('id','url')
 
-        with ThreadPoolExecutor(max_workers=10) as pool:
+        with ThreadPoolExecutor(max_workers=2) as pool:
             id = [author[0] for author in remote_authors_host]
             urls = [author[1] + '/followers' for author in remote_authors_host]
             user, passwd = auth.split(':')
